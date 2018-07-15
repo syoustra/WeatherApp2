f65e4aec40f3a602f4638e2e61218354c0d91818
syoustra <steph.you07@gmail.com>
2018-07-15 14:08:57 -0400

    Finished but never committed

--- app/src/main/java/com/syoustra/weatherapp/api_calls/dark_sky/DarkSkyApi.java
+++ app/src/main/java/com/syoustra/weatherapp/api_calls/dark_sky/DarkSkyApi.java
@@ -4,7 +4,7 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 
-public interface DarkSkyApi {
+public interface  DarkSkyApi {
 
     @GET("{api_key}/{latitude},{longitude}")
     Call<Weather> getWeather (@Path("api_key") String apiKey, @Path("latitude") double latitude, @Path("longitude") double longitude);
